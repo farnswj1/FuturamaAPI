3561c3324aa894627d092de4341ea6a97683f0f2
Justin Farnsworth <fjj12697@gmail.com>
2024-11-19 16:55:24 -0500

    use logging instead of print, move address to variable

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,15 +4,17 @@ use app::get_router;
 use axum::serve;
 use dotenvy::dotenv;
 use tokio::net::TcpListener;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
 
+    let address = "0.0.0.0:8000";
     let router = get_router().await.unwrap();
-    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener = TcpListener::bind(address).await.unwrap();
 
-    println!("LISTENING on 0.0.0.0:8000");
+    info!("LISTENING on {address}");
     serve(listener, router).await.unwrap();
 }
