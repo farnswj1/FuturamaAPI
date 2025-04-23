01392e67a624109894b581c9d92e140c7b2a7ef9
Justin Farnsworth <fjj12697@gmail.com>
2025-04-23 17:29:56 -0400

    refactoring

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navbarBurgers = document.querySelectorAll('.navbar-burger');
+  const active = 'is-active';
 
   // Add a click event on each of them
   navbarBurgers.forEach(el => {
     el.addEventListener('click', () => {
       const target = document.getElementById(el.dataset.target);
-      el.classList.toggle('is-active');
-      target.classList.toggle('is-active');
+      el.classList.toggle(active);
+      target.classList.toggle(active);
     });
   });
 });
